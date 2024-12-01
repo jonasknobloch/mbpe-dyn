7a39a4706f8b795901ce5a43e8e5ea63dd1790e0
Jonas Knobloch <jonas.knobloch@t-online.de>
2024-12-01 20:09:23 +0100

    Add helpers to load reference state

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	stok "github.com/sugarme/tokenizer"
 	sbpe "github.com/sugarme/tokenizer/model/bpe"
@@ -48,17 +49,21 @@ func train() {
 		log.Fatal(err)
 	}
 
-	if err := toFile("vocab.txt", func(writer *bufio.Writer) error {
-		for _, token := range tokenizer.vocab {
-			if _, err := writer.WriteString(token + "\n"); err != nil {
-				return err
-			}
-		}
-
-		return nil
-	}); err != nil {
+	if err := toJSON("vocab.json", tokenizer.atoi); err != nil {
 		log.Fatal(err)
 	}
+
+	// if err := toFile("vocab.txt", func(writer *bufio.Writer) error {
+	// 	for i, token := range tokenizer.vocab {
+	// 		if _, err := writer.WriteString(strconv.Itoa(i) + " " + token + "\n"); err != nil {
+	// 			return err
+	// 		}
+	// 	}
+	//
+	// 	return nil
+	// }); err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	if err := toFile("merges.txt", func(writer *bufio.Writer) error {
 		for _, merge := range tokenizer.merges {
@@ -71,6 +76,24 @@ func train() {
 	}); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func fromFile(name string, callback func(scanner *bufio.Scanner) error) error {
+	file, err := os.Open(name)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	if err := callback(scanner); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func toFile(name string, callback func(writer *bufio.Writer) error) error {
@@ -89,6 +112,42 @@ func toFile(name string, callback func(writer *bufio.Writer) error) error {
 	}
 
 	if err = writer.Flush(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fromJSON(name string, data interface{}) error {
+	file, err := os.Open(name)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	if err := decoder.Decode(data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func toJSON(name string, data interface{}) error {
+	file, err := os.Create(name)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+
+	if err := encoder.Encode(data); err != nil {
 		return err
 	}
 
@@ -136,4 +195,50 @@ func trainReference() {
 	}); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func fromReference(nameVocab, nameMerges string) (*Tokenizer, error) {
+	atoi := make(map[string]int)
+
+	if err := fromJSON(nameVocab, &atoi); err != nil {
+		return nil, err
+	}
+
+	itoa := make(map[int]string, len(atoi))
+
+	for token, idx := range atoi {
+		itoa[idx] = token
+	}
+
+	vocab := make([]string, len(itoa))
+
+	for i := range len(itoa) {
+		vocab[i] = itoa[i]
+	}
+
+	merges := make([][2]string, 0) // unknown number of merges
+
+	if err := fromFile(nameMerges, func(scanner *bufio.Scanner) error {
+		for scanner.Scan() {
+			line := scanner.Text()
+			var merge [2]string
+
+			if _, err := fmt.Sscanf(line, "%s %s", &merge[0], &merge[1]); err != nil {
+				return err
+			}
+
+			merges = append(merges, merge)
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return &Tokenizer{
+		vocab:  vocab,
+		atoi:   atoi,
+		itoa:   itoa,
+		merges: merges,
+	}, nil
 }
