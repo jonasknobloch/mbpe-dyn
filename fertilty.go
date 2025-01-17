ce7f94a20a0f542e123d65bb742a5b3210bcd680
Jonas Knobloch <jonas.knobloch@t-online.de>
2025-01-17 23:46:36 +0100

    Add evaluation tooling

--- fertilty.go
+++ fertilty.go
@@ -1,17 +1,35 @@
 package main
 
-import "fmt"
+type FertilityEvaluator struct {
+	dict *Dict
+}
 
-func Fertility(tokenizer *Tokenizer, dict *Dict) {
+func NewFertilityEvaluator() *FertilityEvaluator {
+	return &FertilityEvaluator{
+		dict: NewDict(),
+	}
+}
+
+func (f *FertilityEvaluator) InitDict(names ...string) error {
+	f.dict.ProcessFiles(names...)
+
+	return nil
+}
+
+func (f *FertilityEvaluator) LoadDict(name string) error {
+	return f.dict.Load(name)
+}
+
+func (f *FertilityEvaluator) Eval(tokenizer *Tokenizer) ([]float64, error) {
 	numTokens := 0
 	numChunks := 0
 
-	for _, chunk := range dict.Items() {
+	for _, chunk := range f.dict.Items() {
 		numTokens += len(tokenizer.model.Tokenize(chunk.src)) * chunk.n
 		numChunks += chunk.n
 	}
 
-	f := float64(numTokens) / float64(numChunks)
+	fertility := float64(numTokens) / float64(numChunks)
 
-	fmt.Println(f, "fertility")
+	return []float64{fertility}, nil
 }
