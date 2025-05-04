89470f7ccf53c7925f644bb7bbc51118b8dd7c5b
Jonas Knobloch <jonas.knobloch@t-online.de>
2025-05-04 20:11:15 +0200

    Inline byte replacements

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.24.0
 
 require (
 	github.com/dlclark/regexp2 v1.11.5
-	github.com/sugarme/tokenizer v0.2.2
 	golang.org/x/image v0.24.0
 	gonum.org/v1/gonum v0.15.1
 	gonum.org/v1/plot v0.15.0
@@ -19,12 +18,6 @@ require (
 	github.com/go-latex/latex v0.0.0-20240709081214-31cef3c7570e // indirect
 	github.com/go-pdf/fpdf v0.9.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/schollz/progressbar/v2 v2.15.0 // indirect
-	github.com/shurcooL/go v0.0.0-20200502201357-93f07166e636 // indirect
-	github.com/shurcooL/go-goon v1.0.0 // indirect
-	github.com/sugarme/regexpset v0.0.0-20200920021344-4d4ec8eaf93c // indirect
 	golang.org/x/text v0.22.0 // indirect
 )
--- utf8.go
+++ utf8.go
@@ -1,9 +1,117 @@
+// Portions of this file are derived from https://github.com/sugarme/tokenizer,
+// licensed under the Apache License, Version 2.0.
+// See https://www.apache.org/licenses/LICENSE-2.0
+
 package mbpe
 
-import "github.com/sugarme/tokenizer/pretokenizer"
+var BytesChar = GenerateBytesChar()
 
-var BytesChar = pretokenizer.BytesChar
-var CharBytes = pretokenizer.CharBytes
+var CharBytes = func() map[string]uint8 {
+	var bc = GenerateBytesChar()
+	var cb = make(map[string]uint8)
+
+	for b, c := range bc {
+		cb[c] = b
+	}
+
+	return cb
+}()
+
+func encodeUTF8(r rune) []byte {
+	const (
+		// first byte of a 2-byte encoding starts 110 and carries 5 bits of data
+		b2Lead = 0xC0 // 1100 0000
+		b2Mask = 0x1F // 0001 1111
+
+		// first byte of a 3-byte encoding starts 1110 and carries 4 bits of data
+		b3Lead = 0xE0 // 1110 0000
+		b3Mask = 0x0F // 0000 1111
+
+		// first byte of a 4-byte encoding starts 11110 and carries 3 bits of data
+		b4Lead = 0xF0 // 1111 0000
+		b4Mask = 0x07 // 0000 0111
+
+		// non-first bytes start 10 and carry 6 bits of data
+		mbLead = 0x80 // 1000 0000
+		mbMask = 0x3F // 0011 1111
+	)
+
+	switch i := uint32(r); {
+	case i <= 1<<7-1: // single byte
+		return []byte{byte(r)}
+	case i <= 1<<11-1: // two bytes
+		return []byte{
+			b2Lead | byte(r>>6),
+			mbLead | byte(r)&mbMask}
+	case i <= 1<<16-1: // three bytes
+		return []byte{
+			b3Lead | byte(r>>12),
+			mbLead | byte(r>>6)&mbMask,
+			mbLead | byte(r)&mbMask}
+	default:
+		return []byte{
+			b4Lead | byte(r>>18),
+			mbLead | byte(r>>12)&mbMask,
+			mbLead | byte(r>>6)&mbMask,
+			mbLead | byte(r)&mbMask}
+	}
+}
+
+func GenerateBytesChar() map[uint8]string {
+	var bc map[uint8]string = make(map[uint8]string)
+
+	n := 0
+
+	// 0 ('Ā') - 32 ('Ġ') - control codes
+	for i := 256; i <= 288; i++ {
+		r := rune(i)
+		b := encodeUTF8(r)
+		bc[uint8(n)] = string(b)
+		n++
+	}
+
+	// 33 ('!') - 126 ('~')
+	for i := 33; i <= 126; i++ {
+		r := rune(i)
+		b := encodeUTF8(r)
+		bc[uint8(n)] = string(b)
+		n++
+	}
+
+	// 127 ('ġ') - 160 ('ł') - control codes
+	for i := 289; i <= 322; i++ {
+		r := rune(i)
+		b := encodeUTF8(r)
+		bc[uint8(n)] = string(b)
+		n++
+	}
+
+	// 161 ('¡') - 172 ('¬')
+	for i := 161; i <= 172; i++ {
+		r := rune(i)
+		b := encodeUTF8(r)
+		bc[uint8(n)] = string(b)
+		n++
+	}
+
+	// 173 - ('Ń') - control code
+	if n == 173 {
+		r := rune(323)
+		b := encodeUTF8(r)
+		bc[uint8(n)] = string(b)
+		n++
+	}
+
+	// 174 ('®') - 255 ('ÿ')
+	for i := 174; i <= 255; i++ {
+		r := rune(i)
+		b := encodeUTF8(r)
+		bc[uint8(n)] = string(b)
+		n++
+	}
+
+	return bc
+}
 
 func Alphabet() []string {
 	alphabet := make([]string, 256)
